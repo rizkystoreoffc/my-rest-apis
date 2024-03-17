0a95340fcb2c730f4493a8d99ac49757a5e2f65a
rizkystoreoffc <160393748+rizkystoreoffc@users.noreply.github.com>
2024-03-17 09:26:56 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'RizzModz'// yourname
+global.MONGO_DB_URI = "mongodb+srv://syakirah3310:N6Eb3way8a1QSUOM@cluster0.7qtn6uk.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "no-reply@rizzpedia.biz.id" //email
+global.email_password = "eRHMrkjX6cTuzgG" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
